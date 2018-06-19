0c5d7bdbe98cb89b63280a9a7fa8aaf290f3fc10
Artem <tema@it4p.ru>
2018-06-19 10:47:04 +0300

    layout updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { routerTransition } from './animations/routerTransition';
 import { HostListener} from '@angular/core';
+import { Location } from '@angular/common';
 
 import 'normalize.css';
 
@@ -21,8 +22,11 @@ export class AppComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private location: Location,
     private router: Router
-  ) {}
+  ) {
+
+  }
 
   ngOnInit() {
     // TODO fix initial search term
@@ -46,5 +50,13 @@ export class AppComponent implements OnInit {
   public resetSearch(): void {
     this.searchWord = '';
     this.router.navigate(['/home']);
+  }
+
+  navigateBack(): void {
+    this.location.back(); // <-- go back to previous location on cancel
+  }
+
+  enabledBack(): boolean {
+    return this.router.url.indexOf('/home') !== 0;
   }
 }
