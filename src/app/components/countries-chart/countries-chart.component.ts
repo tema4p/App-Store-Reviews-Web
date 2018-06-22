945e3f06e8880d38c14604378691df982ec6638b
Alexandr Zyulikov <azyulikov@icloud.com>
2018-06-22 22:12:14 +0600

    temporary fix for hightchart

--- src/app/components/countries-chart/countries-chart.component.ts
+++ src/app/components/countries-chart/countries-chart.component.ts
@@ -2,31 +2,43 @@ import {Component, OnInit, Input, OnChanges, SimpleChanges} from '@angular/core'
 import { Chart } from 'angular-highcharts';
 import COUNTRIES from '../../models/countries.model';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-countries-chart',
   templateUrl: './countries-chart.component.html',
   styleUrls: ['./countries-chart.component.scss']
 })
 export class CountriesChartComponent implements OnInit, OnChanges {
-  @Input()
-  data: any = [];
+  @Input() data: any = [];
 
-  chart: Chart;
+  public chart: Chart;
+  private isReady: boolean = false;
+
   constructor() {
+    this.isReady = true;
     this.renderChart();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.updateChart();
+    if (this.isReady && this.data && this.data.length) {
+      this.updateChart();
+    }
   }
 
   updateChart() {
+    this.isReady = false;
+
     this.chart.removeSerie(0);
     this.chart.addSerie(<Highcharts.SeriesOptions | any> {
       name: 'Reviews',
       colorByPoint: true,
       data: this.data
     });
+
+    setTimeout(() => {
+      this.isReady = true;
+    }, 2000);
   }
 
   renderChart() {
